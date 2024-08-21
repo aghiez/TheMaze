6ea1a99a464d04caef56443ab60097809aa26efc
fiki031 <162071724+fiki031@users.noreply.github.com>
2024-08-21 14:28:18 +0700

    menambahkan asset meja level 2
    Intan Nur Naisyilla

--- Assets/Aset/33 Fikii/folder/AutoScroll.cs
+++ Assets/Aset/33 Fikii/folder/AutoScroll.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class AutoScroll : MonoBehaviour
 {
+    float speed = 100.0f;
+    float textPosBegin = -454.0f;
+    float boundaryTextEnd = -454.0f;
+
+    RectTransform myGorectTransform;
+    [SerializeField]
+    TextMeshProUGUI mainText;
+
+    [SerializeField]
+    bool isLooping = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myGorectTransform =gameObject.GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        IEnumerator AutoScrollText()
+        {
+            while(myGorectTransform.localPosition.y< boundaryTextEnd)
+            {
+                myGorectTransform.Tranlate(Vector3.up * speed * Time.deltaTime);
+                yield return null;
+            }
+        }
 }
