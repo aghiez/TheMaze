5539ef8acb8a2fd898af08e89b3767fb3967ea11
FahmiResing <141643975+azhmalhuda@users.noreply.github.com>
2024-09-30 14:45:12 +0700

    membenarkan button ambil
    isa

--- Assets/Aset/BasicHorrorGameAssets-20240821T015832Z-001/BasicHorrorGameAssets/Scripts/UseChest.cs
+++ Assets/Aset/BasicHorrorGameAssets-20240821T015832Z-001/BasicHorrorGameAssets/Scripts/UseChest.cs
@@ -7,7 +7,8 @@ public class UseChest : MonoBehaviour
     private GameObject OB;
     public GameObject handUI;
     public GameObject objToActivate;
-
+    
+    int a = 0;
 
     private bool inReach;
 
@@ -46,13 +47,25 @@ public class UseChest : MonoBehaviour
     {
 
 
-        if (inReach && Input.GetButtonDown("Interact"))
+        if (inReach && a == 1)
         {
             handUI.SetActive(false);
             objToActivate.SetActive(true);
             OB.GetComponent<Animator>().SetBool("open", true);
             OB.GetComponent<BoxCollider>().enabled = false;
+            a = 0;
         }
     }
+      public void ambil()
+    {
+        a = 1;
+        StartCoroutine(SetAtoZeroAfterDelay(1f)); // Menunggu 1 detik sebelum mengubah a jadi 0
+    }
 
+    // Coroutine untuk mengubah a menjadi 0 setelah 1 detik
+    private IEnumerator SetAtoZeroAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // Tunggu selama 1 detik
+        a = 0; // Setelah 1 detik, ubah nilai a menjadi 0
+    }
 }
